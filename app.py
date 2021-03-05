42b17e80c94d75882b268f46ca661bcb3fb71566
gustavothiodorus10 <gustavothiodorus10@gmail.com>
2021-03-05 13:45:12 +0700

    capstone project fix

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
 from bs4 import BeautifulSoup 
+
 import requests
+import numpy as np
 
 #don't change this
 matplotlib.use('Agg')
@@ -52,10 +54,12 @@ df['metascore'] = df['metascore'].astype('int64')
 @app.route("/")
 def index(): 
 	
-	card_data = f'USD {data["____"].mean().round(2)}'
+	card_data = f'Rating {df["imdb_rating"].mean().round(2)}'
+
+	top7_popular_film = df[['title', 'votes']].sort_values(by='votes', ascending=False).head(7)
 
 	# generate plot
-	ax = ______.plot(figsize = (20,9))
+	ax = top7_popular_film.sort_values(by='votes', ascending=True).plot.barh(x='title', y='votes', figsize = (25,9))
 	
 	# Rendering plot
 	# Do not change this
@@ -63,13 +67,24 @@ def index():
 	plt.savefig(figfile, format='png', transparent=True)
 	figfile.seek(0)
 	figdata_png = base64.b64encode(figfile.getvalue())
-	plot_result = str(figdata_png)[2:-1]
+	votes_result = str(figdata_png)[2:-1]
 
+	top7_highestrating_film = df[['title', 'imdb_rating']].sort_values(by='imdb_rating', ascending=False).head(7)
+	ax_2 = top7_highestrating_film.sort_values(by='imdb_rating', ascending=True).plot.barh(x='title', y='imdb_rating', figsize = (25,9))
+
+	# Rendering plot
+	# Do not change this
+	figfile = BytesIO()
+	plt.savefig(figfile, format='png', transparent=True)
+	figfile.seek(0)
+	figdata_png = base64.b64encode(figfile.getvalue())
+	rating_result = str(figdata_png)[2:-1]
 
 	# render to html
 	return render_template('index.html',
 		card_data = card_data, 
-		plot_result=plot_result
+		votes_result= votes_result,
+		rating_result = rating_result
 		)
 
 
